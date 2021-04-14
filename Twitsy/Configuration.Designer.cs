5d2e02537b9e908cc76630627c1aa40b2517f118
Engbers <dennis.engbers@newayselectronics.com>
2021-04-14 08:17:32 +0200

    Initial revision

---
+++ Twitsy/Configuration.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace Twitsy
+{
+    partial class Configuration
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbEnable = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // cbEnable
+            // 
+            this.cbEnable.AutoSize = true;
+            this.cbEnable.Location = new System.Drawing.Point(27, 25);
+            this.cbEnable.Name = "cbEnable";
+            this.cbEnable.Size = new System.Drawing.Size(59, 17);
+            this.cbEnable.TabIndex = 0;
+            this.cbEnable.Text = "Enable";
+            this.cbEnable.UseVisualStyleBackColor = true;
+            // 
+            // Configuration
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(272, 119);
+            this.Controls.Add(this.cbEnable);
+            this.Name = "Configuration";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox cbEnable;
+    }
+}
+
---
+++ Twitsy/TaskTrayApplicationContext.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+
+namespace Twitsy
+{
+    class TaskTrayApplicationContext : ApplicationContext
+    {
+        NotifyIcon notifyIcon = new NotifyIcon();
+        Configuration configWindow = new Configuration();
+        Boolean Continue = true;
+        Keyboard keyboard = new Keyboard();
+        Thread thread1 = new Thread(Wekker);
+        //Twitsy.Keyboard.ScanCodeShort key = Twitsy.Keyboard.ScanCodeShort.F15;
+
+        public TaskTrayApplicationContext()
+        {
+            MenuItem configMenuItem = new MenuItem("Configuration", new EventHandler(ShowConfig));
+            MenuItem exitMenuItem = new MenuItem("Exit", new EventHandler(Exit));
+
+            notifyIcon.Icon = Properties.Resources.tammy_ico;
+            notifyIcon.DoubleClick += new EventHandler(ShowConfig);
+            notifyIcon.ContextMenu = new ContextMenu(new MenuItem[] { configMenuItem, exitMenuItem });
+            notifyIcon.Visible = true;
+
+            
+            thread1.Start(this);
+        }
+
+      
+        [DllImport("user32.dll")]
+        static extern bool SetCursorPos(int X, int Y);
+
+
+        public static void Wekker(object param )
+        {
+            TaskTrayApplicationContext parrent = (TaskTrayApplicationContext)param;
+            
+                       
+            while (((TaskTrayApplicationContext)parrent).Continue)
+            {
+                //SetCursorPos(500, 500);
+                parrent.keyboard.Send(Twitsy.Keyboard.ScanCodeShort.F15);
+                Thread.Sleep(3 * 1000);
+                //SetCursorPos(600, 600);
+                //parrent.keyboard.Send(Twitsy.Keyboard.ScanCodeShort.F15);
+                //Thread.Sleep(3 * 1000);
+            }
+        }
+
+        void ShowConfig(object sender, EventArgs e)
+        {
+            // If we are already showing the window meerly focus it.
+            if (configWindow.Visible)
+                configWindow.Focus();
+            else
+                configWindow.ShowDialog();
+        }
+
+        void Exit(object sender, EventArgs e)
+        {
+            // We must manually tidy up and remove the icon before we exit.
+            // Otherwise it will be left behind until the user mouses over.
+            notifyIcon.Visible = false;
+            Continue = false;
+            thread1.Abort();
+
+            Application.Exit();
+        }
+    }
+}
